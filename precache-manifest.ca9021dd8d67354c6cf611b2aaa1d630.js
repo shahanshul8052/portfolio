802e38d7936f4aa2abcce256f8ff2ece51dc3601
shahanshul005 <85135432+shahanshul005@users.noreply.github.com>
2024-12-26 02:16:02 -0600

    Updates

---
+++ precache-manifest.ca9021dd8d67354c6cf611b2aaa1d630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd937dbb7646011f0cba98afb86c57b8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a51f841824e47e36d6af",
+    "url": "/portfolio/static/css/main.e058ee08.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/portfolio/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "8e3a127b3eb410b6d841",
+    "url": "/portfolio/static/js/3.445915d0.chunk.js"
+  },
+  {
+    "revision": "a51f841824e47e36d6af",
+    "url": "/portfolio/static/js/main.0ec80b02.chunk.js"
+  },
+  {
+    "revision": "7da939a75b08f78c2536",
+    "url": "/portfolio/static/js/runtime~main.5166ae8e.js"
+  }
+]);
